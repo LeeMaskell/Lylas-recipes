9f6b01157f2a0a92d1669f5b19ac3a978d7ca1c9
Lee Maskell <lmaskellwebdev@gmail.com>
2021-10-08 13:06:46 +1100

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <footer className="page-footer">
-            <p>&copy; {new Date().getFullYear()} <span>SimplyRecipes</span> Built with <a href="https://www.gatsbyjs.com">Gatsby</a></p>
+            <p>&copy; {new Date().getFullYear()} <span>LylasRecipes</span></p>
         </footer>
     )
 }
